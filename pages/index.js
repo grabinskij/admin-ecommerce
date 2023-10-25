f5d83bf10091e960d93604807e74e0176011ea3a
Bogdan Grabinskij <admin@example.com>
2023-10-25 20:12:25 +0200

    Add product adding logic

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
       <h2>
         Hello, <b>{session?.user?.name}</b>
       </h2>
-      <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+      <div className="flex bg-stone-200 gap-1 text-black rounded-lg overflow-hidden">
         <img src={session?.user?.image} alt="" className="w-6 h-6"/>
         <span className="px-2">
           {session?.user?.name}
--- pages/products/new.js
+++ pages/products/new.js
@@ -1,16 +1,25 @@
 import {useState} from "react";
 import axios from "axios";
+import {redirect} from "next/dist/server/api-utils";
+import {useRouter} from "next/router";
 
 
 export default function NewProduct() {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [price, setPrice] = useState('');
-    async function createProduct() {
-        const data = {title, description, price}
-        await axios.post('/api/products', data)
-    }
+    const [goToProducts, setGoToProducts] = useState(false);
+    const router = useRouter()
 
+    async function createProduct(e) {
+        e.preventDefault();
+        const data = {title, description, price}
+        await axios.post('/api/products', data);
+        setGoToProducts(true);
+    }
+    if(goToProducts){
+        router.push('/products');
+    }
     return(
             <form onSubmit={createProduct}>
                 <h1>New product</h1>
