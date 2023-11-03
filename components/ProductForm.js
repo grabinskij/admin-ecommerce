6309429bddb1db22efac018733399ec8acf84844
Bogdan Grabinskij <admin@example.com>
2023-11-03 19:42:14 +0200

    Create api/upload.js

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -9,11 +9,12 @@ export default function ProductForm({
     title: existingTitle,
     description: existingDescription,
     price: existingPrice,
-    images
+    images: existingImages,
 }) {
     const [title, setTitle] = useState(existingTitle || '');
     const [description, setDescription] = useState(existingDescription || '');
     const [price, setPrice] = useState(existingPrice || '');
+    const [images, setImages] = useState(existingImages || []);
     const [goToProducts, setGoToProducts] = useState(false);
     const router = useRouter()
 
@@ -31,6 +32,19 @@ export default function ProductForm({
     if (goToProducts) {
         router.push('/products');
     }
+    async function uploadImages(e) {
+        const files = e.target?.files;
+        if(files?.length > 0) {
+            const data = new FormData();
+            for(const file of files){
+                data.append('file', file)
+            }
+            const res = await axios.post('/api/upload', data);
+                setImages(oldImages => {
+                    return [...oldImages, ...res.data.links];
+            })
+        }
+    }
     return (
         <form onSubmit={saveProduct}>
             <label>Products name</label>
@@ -41,7 +55,22 @@ export default function ProductForm({
                 onChange={e => setTitle(e.target.value)}
             />
             <label>Images</label>
-            <div className="mb-2">
+            <div className="mb-2 flex flex-wrap gap-2">
+                {!!images?.length && images.map(link => (
+                    <div key={link} className="h-24">
+                        <img src={link} alt="img" className="rounded-lg"/>
+                    </div>
+                ))}
+                <label className="w-28 h-28 cursor-pointer border text-center flex flex-col
+                items-center justify-center gap-1 text-sm text-gray-600 rounded-lg">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                         stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round"
+                              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"/>
+                    </svg>
+                    <div>Upload</div>
+                    <input type="file" onChange={uploadImages} className="hidden"/>
+                </label>
                 {!images?.length && (
                     <div>No images in this product</div>
                 )}
---
+++ pages/api/upload.js
@@ -0,0 +1,45 @@
+import multiparty from "multiparty";
+import fs from "fs";
+import {v2 as cloudinary} from 'cloudinary';
+import mime from "mime-types";
+
+
+
+export default async function handle(req, res) {
+   const form = new multiparty.Form();
+   const {fields, files} = await new Promise((resolve, reject) => {
+       form.parse(req, (err, fields, files) => {
+           if(err) reject(err);
+           resolve({fields, files});
+       });
+   });
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_KEY,
+    api_secret: process.env.CLOUD_KEY_SECRET,
+});
+
+const links = [];
+for (const file of files.file) {
+    const ext = file.originalFilename.split('.').pop();
+    const newFilename = Date.now() + '.' + ext;
+    const result = await cloudinary.uploader.upload(
+        file.path, {
+            Key: newFilename,
+            Body: fs.readFileSync(file.path),
+            ACL: 'public-read',
+            ContentType: mime.lookup(file.path),
+        });
+    const link = result.secure_url;
+    links.push(link)
+    console.log('Link:', links)
+    console.log('result:', result)
+}
+return res.json({links});
+}
+
+export const config = {
+    api: {bodyParser: false}
+}
