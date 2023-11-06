af08b55abd8d99ecf14cf8a8797fec1fcf41ff7c
Bogdan Grabinskij <admin@example.com>
2023-11-06 18:41:05 +0200

    Fix bug in api/categories.js

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -13,23 +13,23 @@ export default async function handle(req, res) {
         const {name, parentCategory} = req.body;
         const categoryDoc = await Category.create({
             name,
+            parent: parentCategory || undefined,
+        });
+        res.json(categoryDoc);
+    }
+
+    if (method === 'PUT') {
+        const {name, parentCategory, _id} = req.body;
+        const categoryDoc = await Category.updateOne({_id}, {
+            name,
             parent: parentCategory,
         });
         res.json(categoryDoc);
     }
 
-    // if (method === 'PUT') {
-    //     const {name, parentCategory, _id} = req.body;
-    //     const categoryDoc = await Category.updateOne({_id}, {
-    //         name,
-    //         parent: parentCategory,
-    //     });
-    //     res.json(categoryDoc);
-    // }
-    //
-    // if (method === 'DELETE') {
-    //     const {_id} = req.query;
-    //     await Category.deleteOne({_id});
-    //     res.json('ok');
-    // }
+    if (method === 'DELETE') {
+        const {_id} = req.query;
+        await Category.deleteOne({_id});
+        res.json('ok');
+    }
 }
